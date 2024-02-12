88066d2374eec4f108e754db97e524de04742e13
Dmytro Tkach <dreamworkshow@gmail.com>
2024-02-12 11:33:56 +0100

    prop rey deleted

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -3,7 +3,7 @@ import css from "./FriendListItem.module.css";
 
 export const FriendListItem = ({ friend: { id, avatar, name, isOnline } }) => {
   return (
-    <li className={css.item} key={id} id={id}>
+    <li className={css.item} id={id}>
       <div className={css.border}>
         <img className={css.avatar} src={avatar} alt="avatar" width="48" />
         <p className={css.name}>{name}</p>
